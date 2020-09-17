a63046f45eea9002b1f8dba9270a9380c75c268e
dbesaeva <daliyabesaeva1@gmail.com>
2020-09-17 22:26:28 +0700

    Dialogs beauty

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,21 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div> Dialogs </div>
+        <div className={s.dialogs}>  
+        <div className={s.dialogsItems}>
+            <div className={s.dialog + ' ' + s.active}> Lo </div>
+            <div className={s.dialog}> Daliya </div>
+            <div className={s.dialog}> Timmy </div>
+            <div className={s.dialog}> Ariana </div>
+            <div className={s.dialog}> Helen </div>
+            <div className={s.dialog}> Tom </div>
+        </div>
+        <div className={s.Messages}>
+            <div className={s.Message}> How are you? </div>
+            <div className={s.Message}> ok? </div>
+            <div className={s.Message}> See you </div>
+        </div>
+        </div>
 );
 }
 
