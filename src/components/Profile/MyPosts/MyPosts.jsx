d165a174537cbb0bd561d9be136f9d39ab7c6a4e
dbesaeva <daliyabesaeva1@gmail.com>
2020-09-22 00:42:37 +0700

    taking out the data into index.js

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import c from './MyPosts.module.css'
 import Post from './Post/Post.jsx';
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
-        { likesCount: 12, message: 'hi, how are you' },
-        { likesCount: 19, message: 'its my first post' },
-    ]
-
-    let postsElements = posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />)
+    let postsElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />)
 
     return (<div className={c.postblock}>
         <h3> My Posts</h3>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let posts = [
+  { likesCount: 12, message: 'hi, how are you' },
+  { likesCount: 19, message: 'its my first post' },
+]
+
+let dialogsData = [
+  { id: 1, name: 'Lo' },
+  { id: 1, name: 'Helena' },
+  { id: 1, name: 'Ann' },
+  { id: 1, name: 'Tom' },
+]
+
+let messagesData = [
+  { id: 1, message: 'Hi' },
+  { id: 1, message: 'How are you' },
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App posts={posts} dialogsData={dialogsData} />
   </React.StrictMode>,
   document.getElementById('root')
 );
